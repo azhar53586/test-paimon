bb01d51354b7995f7df3e534d749edc8646f6e96
azhar53586 <azhar34sahilrty@gmail.com>
2025-04-26 16:51:54 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,7 @@
-// Cart functionality
-let cart = [];
-
-function addToCart(productName) {
-  cart.push(productName);
-  alert(${productName} added to cart.);
-}
-
-document.querySelectorAll('.add-to-cart').forEach(button => {
-  button.addEventListener('click', () => {
-    addToCart(button.getAttribute('data-product'));
-  });
-});
-
-// Admin login
 function checkPassword() {
   const password = document.getElementById('password').value;
   if (password === 'admin123') {
-    document.getElementById('admin-content').style.display = 'block';
+    window.location.href = 'admin1.html'; // Go to admin page
   } else {
     alert('Incorrect password!');
   }
